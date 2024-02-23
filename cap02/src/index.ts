158735d6190506602694b029be40f4ae703aab5c
daniel <daniellealuau@gmail.com>
2024-02-23 13:11:49 -0300

    ok

--- cap02/src/index.ts
+++ cap02/src/index.ts
@@ -1,13 +1,14 @@
 // importando as classes necessárias do módulo correspondente
-import { TodoItem } from "./todoItem";
-import { TodoCollection } from "./todoCollection";
+import { TodoItem } from "./todoItem.js";
+import { TodoCollection } from "./todoCollection.js";
+import inquirer from "inquirer"
 
 // criando uma lista de instâncias da classe TodoItem
 let todos: TodoItem[] = [
     new TodoItem(1, "comprar flores"),
     new TodoItem(2, "comprar sapatos"),
-    new TodoItem(3, "retirar ingressos", true),
-    new TodoItem(4, "ligar para joy")
+    new TodoItem(3, "retirar ingressos"),
+    new TodoItem(4, "ligar para joy", true)
 ];
 
 // criando uma instância da classe TodoCollection
@@ -16,6 +17,7 @@ let collection: TodoCollection = new TodoCollection("daniel", todos);
 
 console.clear();
 console.log(`lista de Tarefas de ${collection.userName}`);
+collection.removeComplete();
 collection.getTodoItems(true).forEach(item => item.printDetails())
 
 
