a50eb18bc18cc2744cfc4b20078ff8f1908113d6
daniel <daniellealuau@gmail.com>
2024-02-24 21:29:47 -0300

    função de markar tarefas completas

--- cap02/dist/index.js
+++ cap02/dist/index.js
@@ -1,15 +1,15 @@
-// importando as classes necessárias do módulo correspondente
+// importa as classes necessárias do módulo correspondente
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
 import inquirer from "inquirer";
-// criando uma lista de instâncias da classe TodoItem
+// cria uma lista de instâncias da classe TodoItem
 let todos = [
     new TodoItem(1, "buy flowers"),
     new TodoItem(2, "get shoes"),
     new TodoItem(3, "collect tickets"),
-    new TodoItem(4, "call Joy", true), // a flag 'true' indica que esta tarefa está concluída
+    new TodoItem(4, "call Joy")
 ];
-// criando uma instância da classe TodoCollection
+// cria uma instância da classe TodoCollection
 let collection = new TodoCollection("daniel", todos);
 let showCompleted = true;
 // função para exibir a lista de tarefas no console
@@ -17,26 +17,53 @@ function displayTodoList() {
     // imprime no console o nome do usuário seguido da frase "Lista de Tarefas" e do número de itens incompletos
     console.log(`${collection.userName}'s todo list` +
         `(${collection.getItemCounts().incomplete} itens to do)`);
-    //imprime apenas apenas os incompletos, se a opção de exibir completos estiver desativada
+    // imprime apenas os incompletos, se a opção de exibir completos estiver desativada
     collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
 // enumeração para representar os comandos disponíveis
 var Commands;
 (function (Commands) {
     Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete a Task";
     Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
+// função para adicionar uma nova tarefa
 function promptAdd() {
     console.clear();
-    inquirer.prompt({
+    inquirer
+        .prompt({
         type: "input",
         name: "add",
-        message: "Enter the task: "
-    }).then(answers => {
+        message: "Enter the task: ",
+    })
+        .then((answers) => {
         if (answers["add"] !== "") {
             collection.addTodo(answers["add"]);
         }
+        promptUser();
+    });
+}
+// função para marcar tarefas como concluídas
+function promptComplete() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map((item) => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete,
+        })),
+    })
+        .then((answers) => {
+        let completedTasks = answers["complete"];
+        collection.getTodoItems(true).forEach((item) => {
+            collection.markComplete(item.id, completedTasks.find((id) => id === item.id) !== undefined);
+        });
         promptUser();
     });
 }
@@ -61,11 +88,23 @@ function promptUser() {
             case Commands.Add:
                 promptAdd();
                 break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
         }
     });
 }
 // limpa o console, exibe a lista de tarefas e inicia a interação com o usuário
 console.clear();
 console.log(`lista de tarefas de ${collection.userName}`);
-//collection.removeComplete();  // remove as tarefas concluídas antes de exibir
+// collection.removeComplete();  // remove as tarefas concluídas antes de exibir
 promptUser();
--- cap02/src/index.ts
+++ cap02/src/index.ts
@@ -1,17 +1,17 @@
-// importando as classes necessárias do módulo correspondente
+// importa as classes necessárias do módulo correspondente
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
 import inquirer from "inquirer";
 
-// criando uma lista de instâncias da classe TodoItem
+// cria uma lista de instâncias da classe TodoItem
 let todos: TodoItem[] = [
   new TodoItem(1, "buy flowers"),
   new TodoItem(2, "get shoes"),
   new TodoItem(3, "collect tickets"),
-  new TodoItem(4, "call Joy", true), // a flag 'true' indica que esta tarefa está concluída
+  new TodoItem(4, "call Joy")
 ];
 
-// criando uma instância da classe TodoCollection
+// cria uma instância da classe TodoCollection
 let collection: TodoCollection = new TodoCollection("daniel", todos);
 let showCompleted = true;
 
@@ -22,32 +22,61 @@ function displayTodoList(): void {
     `${collection.userName}'s todo list` +
       `(${collection.getItemCounts().incomplete} itens to do)`
   );
- //imprime apenas apenas os incompletos, se a opção de exibir completos estiver desativada
+  // imprime apenas os incompletos, se a opção de exibir completos estiver desativada
   collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
 
-
 // enumeração para representar os comandos disponíveis
 enum Commands {
-  Add = "Add New Task", 
+  Add = "Add New Task",
+  Complete = "Complete a Task",
   Toggle = "Show/Hide Completed",
-  Quit = "Quit", 
+  Purge = "Remove Completed Tasks",
+  Quit = "Quit",
 }
 
+// função para adicionar uma nova tarefa
 function promptAdd(): void {
-  console.clear()
-  inquirer.prompt({
-    type: "input",
-    name: "add",
-    message: "Enter the task: "
-  }).then(answers => {
-    if(answers["add"] !== "") {
-      collection.addTodo(answers["add"])
-    }
-    promptUser()
-  })
+  console.clear();
+  inquirer
+    .prompt({
+      type: "input",
+      name: "add",
+      message: "Enter the task: ",
+    })
+    .then((answers) => {
+      if (answers["add"] !== "") {
+        collection.addTodo(answers["add"]);
+      }
+      promptUser();
+    });
 }
 
+// função para marcar tarefas como concluídas
+function promptComplete(): void {
+  console.clear();
+  inquirer
+    .prompt({
+      type: "checkbox",
+      name: "complete",
+      message: "Mark Tasks Complete",
+      choices: collection.getTodoItems(showCompleted).map((item) => ({
+        name: item.task,
+        value: item.id,
+        checked: item.complete,
+      })),
+    })
+    .then((answers) => {
+      let completedTasks = answers["complete"] as number[];
+      collection.getTodoItems(true).forEach((item) => {
+        collection.markComplete(
+          item.id,
+          completedTasks.find((id) => id === item.id) !== undefined
+        );
+      });
+      promptUser();
+    });
+}
 
 // função para interagir com o usuário e prompt para escolher opções
 function promptUser(): void {
@@ -68,8 +97,21 @@ function promptUser(): void {
           promptUser();
           break;
         case Commands.Add:
-          promptAdd()
-          break
+          promptAdd();
+          break;
+
+        case Commands.Complete:
+          if (collection.getItemCounts().incomplete > 0) {
+            promptComplete();
+          } else {
+            promptUser();
+          }
+          break;
+
+        case Commands.Purge:
+          collection.removeComplete();
+          promptUser();
+          break;
       }
     });
 }
@@ -77,5 +119,5 @@ function promptUser(): void {
 // limpa o console, exibe a lista de tarefas e inicia a interação com o usuário
 console.clear();
 console.log(`lista de tarefas de ${collection.userName}`);
-//collection.removeComplete();  // remove as tarefas concluídas antes de exibir
+// collection.removeComplete();  // remove as tarefas concluídas antes de exibir
 promptUser();
